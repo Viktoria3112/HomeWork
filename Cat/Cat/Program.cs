8a94767d44dee999ff94f474ab0cb3d48a61a1fc
Viktoria <tori31122000@gmail.com>
2023-03-12 03:27:34 +0700

    Initial project, homework 1

---
+++ Cat/Cat/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cat
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            WriteToConsole(text: "Viktoria ");
+        }
+
+        public static void WriteToConsole(string text)
+        {
+            Console.WriteLine(text + "Hello!");
+        }
+    }
+}
