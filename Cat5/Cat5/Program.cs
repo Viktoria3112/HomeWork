c6794b3119d524b39544fa9f4274d3ca8bdbfff0
Viktoria <tori31122000@gmail.com>
2023-04-17 00:11:06 +0700

    HW6

---
+++ Cat5/Cat5/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.IO.Compression;
+
+namespace Cat4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string startPath = @"C:\Users\tori3\Desktop\программирование\2 семестр\Cat4\HW5";
+            string zipPath = @"C:\Users\tori3\Desktop\программирование\2 семестр\Cat4\HW5.zip";
+            string extractPath = @"C:\Users\tori3\Desktop\программирование\2 семестр\Cat4\newHW5";
+
+            ZipFile.CreateFromDirectory(startPath, zipPath);
+
+            ZipFile.ExtractToDirectory(zipPath, extractPath);
+
+            var directory = new DirectoryInfo(startPath);
+            if (directory.Exists)
+            {
+                Console.WriteLine("Подкатологи:");
+                DirectoryInfo[] dirs = directory.GetDirectories();
+                foreach (DirectoryInfo dir in dirs)
+                {
+                    Console.WriteLine(dir.FullName);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Файлы: ");
+                FileInfo[] files = directory.GetFiles();
+                foreach (FileInfo file in files)
+                {
+                    Console.WriteLine(file.FullName);
+
+                }
+
+
+            }
+            string pathCsvFile = @"C:\Users\tori3\Desktop\программирование\2 семестр\Cat4\Csv.csv";
+            if (File.Exists(pathCsvFile) == false)
+            {
+                Console.WriteLine("Файл не найден: " + pathCsvFile);
+            }
+            else
+            {
+                try
+                {
+                    List<Csv> csv = new List<Csv>
+                {
+                    new Csv{Type="Папка",Name="Cat4",DateOfChange="09.04.23"},
+                    new Csv{Type="Файл",Name="Cin",DateOfChange="09.04.23"}
+
+                };
+                    using (StreamWriter streamReader = new StreamWriter(pathCsvFile))
+                    {
+                        using (CsvWriter csvReader = new CsvWriter(streamReader))
+                        {
+                            csvReader.Configuration.Delimiter = "\t";
+                            csvReader.WriteRecords(csv);
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            }
+            try
+            {
+                File.Delete(zipPath);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Не удалось удалить: {0}");
+            }
+
+            string pathCsvFile1 = Environment.ExpandEnvironmentVariables(@"%AppData%/Lesson12Homework.txt");
+            Directory.CreateDirectory(pathCsvFile1);
+        }
+
+    }
+}
---
+++ Cat5/Cat5/Program2.cs
@@ -0,0 +1,43 @@
+﻿using Cat4;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cat5
+{
+    internal class Program2
+    {
+        static async void Main(string[] args)
+        {
+
+            var fileName = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Lesson12Homework");
+            if (File.Exists(fileName) == false)
+            {
+                Console.WriteLine("Файл не найден: " + fileName);
+            }
+            else
+            {
+                try
+                {
+                    using (StreamReader reader = new StreamReader(fileName))
+                    {
+                        string file = await reader.ReadToEndAsync();
+                        Console.WriteLine(file);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            
+            
+
+        }
+    }
+}
+
